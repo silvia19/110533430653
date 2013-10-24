0fbbfbe868d29b62d469f71562452c6846f39263
Silvia Wahyuningrum <silviawahyuningrum1905@gmail.com>
2013-10-25 00:05:40 +0700

    cookie dan session
    cookie dan session

---
+++ cookie_session/web/latihan1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 4.0 Transitional//EN"> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head> 								
+	<title>Identifikasi Metode</title> 	<!--digunakan untuk menulis judul -->		
+</head>
+
+<body>
+<?php
+echo $_GET ['nama'] $_GET ['genre']
+?>
+
+</body>
+</html>
